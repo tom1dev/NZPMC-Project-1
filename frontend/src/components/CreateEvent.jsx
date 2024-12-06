ea9397bbcc8a447644ba5f66908be4d66773e771
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-07 03:11:44 +1300

    created view popup for viewing a users details

---
+++ frontend/src/components/CreateEvent.jsx
@@ -0,0 +1,14 @@
+
+const CreateEvent = () => {
+    return (
+        <div>
+            <h1>Create Event</h1>
+        </div>
+    )
+
+
+
+
+}
+
+export default CreateEvent;
---
+++ frontend/src/components/UserDetailsPopup.jsx
@@ -0,0 +1,29 @@
+import style from '../styles/UserPopup.module.css';
+
+
+const UserDetailsPopup = ({ showPopup, togglePopup, user}) => {
+
+    return (
+        <div className={style.popupWindow} onClick={togglePopup}>
+            <div className={style.popupContainer}>
+                
+                    <h className={style.popupTitle}>User Details</h>
+                    <h2 className={style.popupName}>Name: {user.name}</h2>
+                    <h2 className={style.popupEmail}>Email: {user.email}</h2>
+                    <h2 className={style.popupEmail}>Concerts:</h2>
+
+
+                    <button className={style.popupCloseButton} onClick={togglePopup}>Close</button>
+            </div>
+
+        </div>
+
+    );
+
+
+
+
+
+}
+
+export default UserDetailsPopup;
