78c0d8af46c29d6d4303a95c325f182d673907bd
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-06 14:12:38 +1300

    created dummy login page format

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -2,23 +2,9 @@ import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Admin">admin</Link>
-          </li>
-          <li>
-            <Link to="/Signin">signin</Link>
-          </li>
-        </ul>
-      </nav>
+
 
       <Outlet />
-    </>
   )
 };
 
