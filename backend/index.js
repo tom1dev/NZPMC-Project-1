817d99d2c52b7057124ad65f24dce10481dc3e4b
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-08 18:30:40 +1300

    added express and nodemon to the project.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,7 @@
-console.log('hi')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
