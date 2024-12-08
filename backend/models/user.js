59cf536cd830b1f71a4adfb967f3ddbc90e11859
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-08 20:08:50 +1300

    created user, event and usersevent schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
-    
-  
-mongoose.set('strictQuery',false)
-  
-mongoose.connect(url).then(result =>{ console.log("connected to mongoDB")}).catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-})
-
 const userSchema = new mongoose.Schema({
+    id: String,
     name: String,
     email: String,
     passwordHash: String,
     admin: Boolean,
 })
-  
-  const Note = mongoose.model('Note', noteSchema)
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
