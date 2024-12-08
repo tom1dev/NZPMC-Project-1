099833b9b590a936c7a0c851785cd7a16a8c4c3c
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-08 18:57:28 +1300

    added .env library

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 app.use(express.json())
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
+})
+
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI;
+    
+  
+mongoose.set('strictQuery',false)
+  
+mongoose.connect(url).then(result =>{ console.log("connected to mongoDB")}).catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
   })
