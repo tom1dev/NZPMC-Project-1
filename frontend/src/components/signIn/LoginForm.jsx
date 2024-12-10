417fdcda9c4c7ed7938f294f02fea9f82c37e88a
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-10 19:06:57 +1300

    refactored the signin page and its corresponding components:

--- frontend/src/components/signIn/LoginForm.jsx
+++ frontend/src/components/signIn/LoginForm.jsx
@@ -1,146 +1,79 @@
 import styleSignin from '../../styles/Signin.module.css';
-import {useState, useEffect} from 'react';
-import signinService from '../../services/signInService.js';
-import userService  from '../../services/userService.js';
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import signinService from '../../services/signinService';
+import userService from '../../services/userService';
+import { useNavigate } from 'react-router-dom';
 
 
-const LoginForm = ({title,isSignup,setIsSignup}) => {
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const [name,setName] = useState('');
-    const [error,setError] = useState(false);
+const LoginForm= ({ isSignup,setError})=> {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
     const navigate = useNavigate();
-
-
-    useEffect(() => {
-        const fetchUserInformation = async () => {
-            try{
-                const user = await userService.getUserByToken();
-                return user[0];
-            }catch (error){
-                console.log(error);
-                return;
-            }
-                
-        }
-
-        const fetchAndNavigate = async () => {
-            const user = await fetchUserInformation();
-            if (user && user.email === "admin") {
-                navigate("/admin");
-            } else if (user) {
-                navigate("/");
-            }
-        };
-
-
-        fetchAndNavigate();
-    }, []);
-
-    const handleEmailChange = (event) =>{
-        setEmail(event.target.value)
+    
+    const handleParramChange = (event,setter) => {
+        setter(event.target.value)
     }
 
-    const handleNameChange = (event) =>{
-        setName(event.target.value)
-    }
-
-
-    const handlePasswordChange = (event) =>{
-        setPassword(event.target.value)
-    }
-
-
-
-    const onSignupClicked = () => {
-        console.log("Sign up Clicked")
-        setIsSignup(!isSignup);
-
-    }
     const handleSubmit = async (event) => {
         event.preventDefault();
         if (!email || !password || (isSignup && !name)) {
             alert("Please fill out all fields.");
             return;
         }
-        try{
-            if(isSignup){
-                    const user = {name,email,passwordHash: password,isAdmin:false};
-                    const data = await userService.createUser(user);
+        try {
+            if (isSignup) {
+                const user = { name, email, passwordHash: password, isAdmin: false };
+                const data = await userService.createUser(user);
 
-                    document.cookie = `token=${"Bearer "+data}`;
-                    navigate("/");
+                document.cookie = `token=${"Bearer " + data}`;
+                navigate("/");
 
                 //handle submition logic with correct service
-            }else{
-                    const data = await signinService.signIn(email, password);
-                    document.cookie = `token=${"Bearer "+ data}`;
-                    if(email === "admin"){
-                        navigate("/admin");
-                    }else{
-                        navigate("/");
-                    }
+            } else {
+                const data = await signinService.signIn(email, password);
+                document.cookie = `token=${"Bearer " + data}`;
+                if (email === "admin") {
+                    navigate("/admin");
+                } else {
+                    navigate("/");
+                }
 
             }
-                
-        }catch(error){
-                setError(true);
-                console.log(error);
+
+        } catch (error) {
+            setError(true);
+            console.log(error);
+        }
+    }
+    
+    
+    return <form onSubmit={handleSubmit}>
+
+        {isSignup &&
+            <div className={styleSignin.parrameterBox}>
+                <h2>Name</h2>
+                <input value={name} onChange={event => handleParramChange(event,setName)} />
+            </div>
         }
 
 
-
-    }
-
-    return(
-        <div className={styleSignin.background}>
-            <h className={styleSignin.header}>{title}</h>
-            {  
-                
-                error && <h4 className={styleSignin.error}>{isSignup ? "Account couldn't be created": "Password or email incorrect"}</h4>
-                    
-               
-            }
-            <form onSubmit={handleSubmit}>
-                
-                {isSignup && 
-                    <div className={styleSignin.parrameterBox}>
-                        <h2>Name</h2> 
-                        <input value={name}  onChange={handleNameChange}/>
-                    </div>
-                }
-
-
-                <div className={styleSignin.parrameterBox}>
-                    <h2>Email</h2>
-                    <input value={email}  onChange={handleEmailChange}/>
-                </div>
-                <div className={styleSignin.parrameterBox}>
-                    <h2>Password</h2> 
-                    <input value={password}  onChange={handlePasswordChange}/>
-                </div>
-
-
-                {isSignup ?<button type="submit">Sign Up</button>:<button type="submit">Login</button>}
-                
-
-
-            </form>
-
-
-
-            <div className={styleSignin.linkBox}>
-                    {   isSignup?
-                        <><p>Have an account?</p>
-                        <p className={styleSignin.signupLink} onClick={onSignupClicked}>  Sign In</p></>:
-                        <><p>Don't have an account?</p>
-                        <p className={styleSignin.signupLink} onClick={onSignupClicked}>  Sign Up</p></>
-                    }
-            </div>        
+        <div className={styleSignin.parrameterBox}>
+            <h2>Email</h2>
+            <input value={email} onChange={event => handleParramChange(event,setEmail)} />
         </div>
-    )
+        <div className={styleSignin.parrameterBox}>
+            <h2>Password</h2>
+            <input value={password} onChange={event => handleParramChange(event,setPassword)} />
+        </div>
 
+
+        {isSignup ? <button type="submit">Sign Up</button> : <button type="submit">Login</button>}
+
+
+
+    </form>;
 }
 
 export default LoginForm;
